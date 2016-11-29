6f0d4f93bf8a0ac2f6ad55bf8bb40a0400a523ec
BerniceChua <Bernice.Chua.415@gmail.com>
2016-11-29 14:04:29 -0800

    Experiment With Video.

---
+++ Assets/360VRCameraCaptureRig/Editor/LS360VRCameraEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using LSToolKit;
+
+[CustomEditor(typeof(LS360VRCamera))]
+public class LS360VRCameraEditor : Editor {
+
+    private GUIStyle boxStyle;
+    Texture WorldIcon = (Texture)Resources.Load("LS_Logo");
+
+    public override void OnInspectorGUI()
+    {
+        LS360VRCamera targetCamScript = (LS360VRCamera)target;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Box(WorldIcon, GUILayout.Width(WorldIcon.width), GUILayout.Height(WorldIcon.height), GUILayout.ExpandWidth(true));
+        GUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+
+        base.OnInspectorGUI();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+
+
